ff3251c8d98af26913c25a37c96611c3401ded90
shahadathossain26 <shiimran172372@gmail.com>
2023-04-05 12:39:27 +0600

    server created

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send("Todo list server is running")
+})
+app.listen(port, () => console.log(`Todo list server is running on ${port}`))
+
